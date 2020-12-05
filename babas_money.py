ebbe0c3fc1a94c30fedbb84d858d7bcb2553c6b5
Drae3213 <67166869+Drae3213@users.noreply.github.com>
2020-12-05 11:39:57 -0800

    added babas money

---
+++ babas_money.py
@@ -0,0 +1,23 @@
+"""
+p = principle (present value)
+r = interest rate
+n = how many payments per period
+t = how many periods
+pv = future value
+
+"""
+p = float(input("What's the principle: "))
+r = float(input("What's the rate: "))
+n = int(input("how many periods: "))
+t = int(input("How many payments per period: "))
+pv = p * (pow((1 + r/100/n),n * t))
+          
+print (pv)
+
+def compund_interest(p, r, n ,t):
+    balance = p * (pow(1 + r/100/n, n * t ))
+    ci = balance - p
+    print("Balance at the end of the period ",round(balance,2))
+    print("Interest earned is ",round(ci,2))
+
+compound_interest(5000, 10, 12, 5)
