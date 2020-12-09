6472258eecb86f17b2066f7b9bf897e1b50b908a
Drae3213 <67166869+Drae3213@users.noreply.github.com>
2020-12-08 20:42:19 -0800

    babas money,addedn roi(return on investment), calculation.

--- babas_money.py
+++ babas_money.py
@@ -1,23 +1,43 @@
 """
 p = principle (present value)
 r = interest rate
-n = how many payments per period
-t = how many periods
-pv = future value
+n = num of compound periods
+t = num of years
+balance = total @ the end time
+earnings = total amount earned
+roi = return on investment 
 
 """
-p = float(input("What's the principle: "))
-r = float(input("What's the rate: "))
-n = int(input("how many periods: "))
-t = int(input("How many payments per period: "))
-pv = p * (pow((1 + r/100/n),n * t))
-          
-print (pv)
+
 
 def compound_interest(p, r, n ,t):
+    p = float(input("What's the principle: $ "))
+    r = float(input("What's the rate? % = "))
+    n = int(input("how many periods per year/ : "))
+    t = int(input("How many years? "))
+
     balance = p * (pow((1 + r/100/n), n * t ))
-    ci = balance - p
-    print("Balance at the end of the period ",round(balance,2))
-    print("Interest earned is ",round(ci,2))
+    earnings = balance - p
+    roi = earnings / balance * 100 
+
+    print("\n", "You earned $", round (earnings, 2), "during the",t,"year period. ")
+    print("Balance at the end of the period ",round(balance,2), "\n")
+    print("Interest earned is ",round(roi,2), "% returned on investment.")
 
 compound_interest(5000, 10, 12, 5)
+
+"""
+interest = x * balance
+
+12.68 = x * 112.68
+
+12.68/112.68 = x * 112.68/112.68  
+
+x = 0.11255 
+
+x = 100 * 0.12555  
+
+x = 11.255 and/or x = 11.26%
+
+roi = return on investment = 11.26%
+"""
