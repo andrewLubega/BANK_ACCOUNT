874c661d4a087e3bfeaa5eb0cff41effbe535593
Drae3213 <67166869+Drae3213@users.noreply.github.com>
2020-12-11 17:14:22 -0800

    babas money, added class Bank_Account

--- babas_money.py
+++ babas_money.py
@@ -8,23 +8,60 @@ earnings = total amount earned
 roi = return on investment 
 
 """
+class Bank_Account:
+    def __init__(self,balance,name):
+        self.balance = balance
+        self.name = name
+        print("My balance is 0")
+    def withdraw(self):
+        amount = float(input("Enter amount to be withdrawn: "))
+
+        if self.balance >= amount:
+            self.balance -= amount
+            print("\n You withdrew:", amount)
+        else:
+            print("\n Insufficient principle ")
+
+    def display(self):
+        print(self.name, ": \t Net Available principle=", self.balance)
+
+    def compound_interest(self, p, r, n ,t):
+        p = float(input("What's the principle: $ "))
+        r = float(input("What's the rate? % = "))
+        n = int(input("how many periods per year: "))
+        t = int(input("How many years? "))
+
+        balance = p * (pow((1 + r/100/n), n * t ))
+        earnings = balance - p
+        roi = earnings / balance * 100 
+
+        print("\n", "You earned $", round (earnings,2), "during the",t,"year period. ")
+        print("Balance at the end of the period ",round(balance,2), "\n")
+        print("Interest earned is ",round(roi,2), "% returned on investment.")
+
+    def mortgage_payment(self, n, rate):
+        p = self.balance * (rate/12/100 * pow((1 + rate/12/100),n))/(pow((1 + rate/12/100),n)-1)
+
+# Driver code
+
+gabe_account = Bank_Account(318200, "gabe")
+print(gabe_account.display())
+# s.mortgage_payment(360, 2.875)
+
+# s.compound_interest(5000, 10, 12, 5)
+
+andrew_account = Bank_Account(7000000000000, "rich")
+print(andrew_account.display())
+
+aris_account = Bank_Account(0, "broke")
+print(aris_account.display())
+
+jacore_account = Bank_Account(100000, "jacore")
+print(jacore_account.display())
 
 
-def compound_interest(p, r, n ,t):
-    p = float(input("What's the principle: $ "))
-    r = float(input("What's the rate? % = "))
-    n = int(input("how many periods per year/ : "))
-    t = int(input("How many years? "))
 
-    balance = p * (pow((1 + r/100/n), n * t ))
-    earnings = balance - p
-    roi = earnings / balance * 100 
 
-    print("\n", "You earned $", round (earnings, 2), "during the",t,"year period. ")
-    print("Balance at the end of the period ",round(balance,2), "\n")
-    print("Interest earned is ",round(roi,2), "% returned on investment.")
-
-compound_interest(5000, 10, 12, 5)
 
 """
 interest = x * balance
